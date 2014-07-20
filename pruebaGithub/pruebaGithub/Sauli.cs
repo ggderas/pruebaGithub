12c89df41159de4248b73e135c0956b862054f1a
Luis Manuel Deras <lmrderas@gmail.com>
2014-07-19 21:51:01 -0600

    Merge pull request

---
+++ pruebaGithub/pruebaGithub/Sauli.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaGithub{
+    class Sauli{
+        public String Dormir(bool estoyEnDiseño) {
+            if (estoyEnDiseño) return "Ha dormir zZz";
+            return "Sigo despierto";
+        }
+    }
+}
